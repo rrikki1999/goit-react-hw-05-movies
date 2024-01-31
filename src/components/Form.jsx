6efecdf9767f161b2d8717b60a40b8ae5afc724a
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2024-01-31 19:52:51 +0200

    hw 5

---
+++ src/components/Form.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from '../css/Movies.module.css';
+
+const Form = ({ onClick }) => {
+  return (
+    <form onClick={e => onClick(e)}>
+      <div className={styles.searchContainer}>
+        <input
+          type="text"
+          placeholder="Enter movie title"
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          onKeyPress={handleKeyPress}
+          className={styles.searchInput}
+        />
+        <button className={styles.searchButton}>Search</button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,57 +1,58 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Form, useSearchParams } from 'react-router-dom';
 import { searchMovies } from '../services/api';
 import styles from '../css/Movies.module.css';
 import { MoviesList } from '../components/MoviesList';
+import { Loader } from 'components/Loader';
 
 const Movies = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  const [error, setError] = useState(null);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [previousQuery, setPreviousQuery] = useState('');
+  const [error, setError] = useState('');
 
-  const handleSearch = useCallback(async (query = '') => {
-    try {
-      const response = await searchMovies(query);
-      setSearchResults(response.data.results);
-      setSearchParams({ query }); 
-    } catch (error) {
-      setError(error);
-    }
-  }, [setSearchParams]);
+  const query = searchParams.get('sQuery');
 
   useEffect(() => {
-    const queryParam = searchParams.get('query');
-    if (queryParam) {
-      setSearchQuery(queryParam);
-      handleSearch(queryParam);
-    }
-  }, [searchParams, handleSearch]);
+    const searchMovie = async () => {
+      try {
+        setIsLoading(true);
+        if (query && query.trim() !== '') {
+          setMovies([]);
+          setPreviousQuery(query);
+          const data = await searchMovies(query);
+          if (Array.isArray(data.results)) {
+            setMovies(data.results);
+          }
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  const handleKeyPress = e => {
-    if (e.key === 'Enter') {
-      handleSearch(searchQuery);
-    }
+    searchMovie();
+  }, [query]);
+
+  const searchSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.currentTarget.elements.query.value.trim();
+    setSearchParams({
+      sQuery: searchValue,
+    });
   };
 
   return (
     <div className={styles.moviesContainer}>
       <h1>Search Movies</h1>
+      {isLoading && <Loader />}
       {error && <p>Something went wrong...</p>}
-      <div className={styles.searchContainer}>
-        <input
-          type="text"
-          placeholder="Enter movie title"
-          value={searchQuery}
-          onChange={e => setSearchQuery(e.target.value)}
-          onKeyPress={handleKeyPress}
-          className={styles.searchInput}
-        />
-        <button onClick={() => handleSearch(searchQuery)} className={styles.searchButton}>
-          Search
-        </button>
-      </div>
-      {searchResults.length > 0 && <MoviesList movies={searchResults} />}
+      
+
+      <Form onClick={searchSubmit}/>
+      {movies.length > 0 && <MoviesList movies={movies} />}
     </div>
   );
 };
